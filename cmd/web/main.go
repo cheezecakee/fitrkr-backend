b74e4e8da2ce765d3972e4831049cb1c17fedff9
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-03 16:59:08 -0300

    feat(cmd): add main.go to start web server

---
+++ cmd/web/main.go
@@ -0,0 +1,34 @@
+// Package main
+package main
+
+import (
+	"github.com/cheezecakee/logr"
+	"github.com/joho/godotenv"
+
+	"github.com/cheezecakee/fitrkr-backend/internal/adapters/primary/web"
+	"github.com/cheezecakee/fitrkr-backend/internal/adapters/secondary/db/postgres"
+	"github.com/cheezecakee/fitrkr-backend/internal/core/services/users"
+)
+
+func main() {
+	logr.Init(&logr.PlainTextFormatter{}, logr.LevelInfo, nil)
+
+	if err := godotenv.Load(); err != nil {
+		logr.Get().Errorf("No .env file found: %v", err)
+	}
+
+	db := postgres.NewPostgresConn()
+	defer db.Close()
+
+	userRepo, err := postgres.NewUserRepo(db)
+	if err != nil {
+		logr.Get().Errorf("failed to init postgres user repo: %v", err)
+	}
+
+	userService := users.NewService(userRepo)
+
+	server := web.NewApp(userService, web.WithPort(8000))
+
+	logr.Get().Info("Starting server...")
+	server.Run()
+}
