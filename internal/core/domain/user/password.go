c247464ffd48a8619ba0768017b5b6243243659f
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-04 08:26:29 -0300

    feat(user): add password validation

--- internal/core/domain/user/password.go
+++ internal/core/domain/user/password.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"errors"
+	"regexp"
 
 	"github.com/cheezecakee/fitrkr-backend/pkg/helper"
 )
@@ -9,17 +10,43 @@ import (
 type Password string
 
 var (
-	ErrEmptyPassword    = errors.New("empty password")
-	ErrPasswordTooShort = errors.New("password too short")
-	ErrPasswordTooLong  = errors.New("password too long")
+	ErrEmptyPassword     = errors.New("empty password")
+	ErrPasswordTooShort  = errors.New("password too short")
+	ErrPasswordTooLong   = errors.New("password too long")
+	ErrPasswordNoChar    = errors.New("password must contain a character")
+	ErrPasswordNoDigit   = errors.New("password must contain a digit")
+	ErrPasswordNoSpecial = errors.New("password must contain a special character")
+)
+
+var (
+	hasLetterRegex = regexp.MustCompile(`[a-zA-Z]`)
+	hasDigitRegex  = regexp.MustCompile(`[0-9]`)
+	specialRegex   = regexp.MustCompile(`[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]`)
 )
 
 func NewPassword(password string) (Password, error) {
+	if password == "" {
+		return "", ErrEmptyPassword
+	}
+
 	if len(password) < 8 {
 		return "", ErrPasswordTooShort
 	}
+
 	if len(password) > 16 {
 		return "", ErrPasswordTooLong
+	}
+
+	if !hasLetterRegex.MatchString(password) {
+		return "", ErrPasswordNoChar
+	}
+
+	if !hasDigitRegex.MatchString(password) {
+		return "", ErrPasswordNoDigit
+	}
+
+	if !specialRegex.MatchString(password) {
+		return "", ErrPasswordNoSpecial
 	}
 
 	hashPass, err := helper.HashPassword(password)
