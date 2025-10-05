73c512627f68c5ddfc747bd419cbff3abf82b3c3
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-05 12:27:22 -0300

    refactor(ports): extend user port for additional services

--- internal/ports/user.go
+++ internal/ports/user.go
@@ -8,8 +8,15 @@ import (
 	"github.com/cheezecakee/fitrkr-backend/internal/core/domain/user"
 )
 
-var ErrUserNotFound = errors.New("user does not exist")
+var (
+	ErrUserNotFound      = errors.New("user does not exist")
+	ErrDuplicateEmail    = errors.New("email already exists")
+	ErrDuplicateUsername = errors.New("username already exists")
+)
 
 type UserRepo interface {
 	Add(ctx context.Context, u user.User) error
+	GetByUsername(ctx context.Context, username string) (*user.User, error)
+	GetByEmail(ctx context.Context, email string) (*user.User, error)
+	GetByID(ctx context.Context, id string) (*user.User, error)
 }
