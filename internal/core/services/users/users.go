37e252a26b9722076d49f2c3913bd3650bc6fe9d
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-05 12:27:21 -0300

    feat(users): add get user use cases

--- internal/core/services/users/users.go
+++ internal/core/services/users/users.go
@@ -3,12 +3,22 @@ package users
 
 import (
 	"context"
+	"errors"
 
 	"github.com/cheezecakee/fitrkr-backend/internal/ports"
 )
 
+var (
+	ErrUserNotFound      = errors.New("user does not exist")
+	ErrDuplicateEmail    = errors.New("email already exists")
+	ErrDuplicateUsername = errors.New("username already exists")
+)
+
 type UserService interface {
 	CreateAccount(context.Context, CreateAccountReq) (*CreateAccountResp, error)
+	GetByID(ctx context.Context, req GetUserByIDReq) (*GetUserResp, error)
+	GetByUsername(ctx context.Context, req GetUserByUsernameReq) (*GetUserResp, error)
+	GetByEmail(ctx context.Context, req GetUserByEmailReq) (*GetUserResp, error)
 }
 
 type Service struct {
