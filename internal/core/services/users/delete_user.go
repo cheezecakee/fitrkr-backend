b4ac37e3c2c419817e80562580c5b4d91037e814
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-05 15:26:05 -0300

    feat(user): add delete user use case

---
+++ internal/core/services/users/delete_user.go
@@ -0,0 +1,25 @@
+package users
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cheezecakee/logr"
+
+	"github.com/cheezecakee/fitrkr-backend/internal/ports"
+)
+
+func (s *Service) Delete(ctx context.Context, req string) error {
+	err := s.userRepo.Delete(ctx, req)
+	if err != nil {
+		if err == ports.ErrUserNotFound {
+			logr.Get().Error("user not found")
+			return ErrUserNotFound
+		}
+		logr.Get().Errorf("failed to delete user: %v", err)
+		return fmt.Errorf("failed to delete user: %w", err)
+	}
+
+	logr.Get().Info("User deleted successfully")
+	return nil
+}
