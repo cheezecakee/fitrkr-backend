d71b78f3218ac986ecd7a2684346d7b180905e85
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-05 12:26:11 -0300

    refactor(web): update server setup with registry pattern

--- internal/adapters/primary/web/server.go
+++ internal/adapters/primary/web/server.go
@@ -8,30 +8,32 @@ import (
 	"github.com/go-chi/chi/v5"
 
 	v1 "github.com/cheezecakee/fitrkr-backend/internal/adapters/primary/web/v1"
+	"github.com/cheezecakee/fitrkr-backend/internal/adapters/primary/web/v1/handlers"
 	"github.com/cheezecakee/fitrkr-backend/internal/core/services/users"
 )
 
 type App struct {
-	chi         *chi.Mux
-	userService users.UserService
-	port        int
+	chi      *chi.Mux
+	registry *handlers.HandlerResgistry
+	port     int
 }
 
 func NewApp(userService users.UserService, opts ...AppOption) *App {
 	app := &App{
-		userService: userService,
-		port:        8000,
-		chi:         chi.NewRouter(),
+		port: 8000,
+		chi:  chi.NewRouter(),
 	}
 
 	for _, applyOption := range opts {
 		applyOption(app)
 	}
 
+	app.registry = handlers.NewHandlerRegistry(userService)
+
 	fs := http.FileServer(http.Dir("internal/adapters/primary/web/docs"))
 	app.chi.Handle("/api/v1/docs/*", http.StripPrefix("/api/v1/docs/", fs))
 
-	app.chi.Mount("/api/v1", v1.InitAppRoutes(userService))
+	app.chi.Mount("/api/v1", v1.RegisterRoutes(app.registry))
 
 	return app
 }
