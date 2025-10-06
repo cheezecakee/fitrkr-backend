9fe3ccc04cad0150d8aab92b430bd51c641a4b6a
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-06 15:42:04 -0300

    chore(helper): move password-related functions to password domain

--- pkg/helper/helper.go
+++ pkg/helper/helper.go
@@ -1,11 +1,6 @@
 // Package helper
 package helper
 
-import (
-	"github.com/cheezecakee/logr"
-	"golang.org/x/crypto/bcrypt"
-)
-
 func Clamp(value, min, max int) int {
 	switch {
 	case value < min:
@@ -15,21 +10,4 @@ func Clamp(value, min, max int) int {
 	default:
 		return value
 	}
-}
-
-func HashPassword(password string) (string, error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		logr.Get().Errorf("failed to hash password: %v", err)
-		return "", err
-	}
-	return string(hashedPassword), nil
-}
-
-func ComparePassword(hash, password string) error {
-	if err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)); err != nil {
-		logr.Get().Errorf("failed to compare password: %v", err)
-		return err
-	}
-	return nil
 }
