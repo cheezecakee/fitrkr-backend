b711535fd0f45667475de092ff7c81fa7dc77dfa
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-06 23:29:54 -0300

    feat(user): add height domain

---
+++ internal/core/domain/user/height.go
@@ -0,0 +1,65 @@
+package user
+
+import "errors"
+
+var (
+	ErrNegativeHeight    = errors.New("height cannot be negative")
+	ErrInvalidHeightUnit = errors.New("invalid height unit")
+)
+
+type Height struct {
+	value float64
+	unit  HeightUnit
+}
+
+type HeightUnit string
+
+const (
+	Cm   HeightUnit = "cm"
+	FtIn HeightUnit = "ft_in"
+)
+
+func NewHeight(value float64, unit HeightUnit) (Height, error) {
+	if value < 0 {
+		return Height{}, ErrNegativeHeight
+	}
+	if unit != Cm && unit != FtIn {
+		return Height{}, ErrInvalidHeightUnit
+	}
+	return Height{
+		value: value,
+		unit:  unit,
+	}, nil
+}
+
+func NewHeightFtIn(feet int, inches float64) (Height, error) {
+	totalInches := float64(feet)*12 + inches
+	return NewHeight(totalInches, FtIn)
+}
+
+func (h Height) ToCm() float64 {
+	if h.unit == Cm {
+		return h.value // already cm
+	}
+
+	return h.value * 2.54
+}
+
+func (h Height) ToFtIn() (feet int, inches float64) {
+	totalInches := h.value
+	if h.unit == Cm {
+		totalInches = h.value / 2.54
+	}
+
+	feet = int(totalInches / 12)
+	inches = totalInches - float64(feet*12)
+	return feet, inches // ft_in
+}
+
+func (h Height) Unit() HeightUnit {
+	return h.unit
+}
+
+func (h Height) Value() float64 {
+	return h.value
+}
