adf90fb766a3437474511268321c836b5e772821
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-07 16:29:05 -0300

    fix(user): fix validation logic in subscription period domain

---
+++ internal/core/domain/user/period.go
@@ -0,0 +1,28 @@
+package user
+
+import (
+	"errors"
+	"strings"
+)
+
+var ErrInvalidBillingPeriod = errors.New("invalid billing period")
+
+type Period string
+
+const (
+	Monthly Period = "monthly"
+	Yearly  Period = "yearly"
+)
+
+func NewBillingPeriod(period string) (Period, error) {
+	period = strings.ToLower(period)
+
+	switch period {
+	case "monthly":
+		return Monthly, nil
+	case "yearly":
+		return Yearly, nil
+	default:
+		return "", ErrInvalidBillingPeriod
+	}
+}
