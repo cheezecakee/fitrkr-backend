cbe0a0c9ac49a8c981d9c1de6be7f1cd5b51c0ce
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-06 23:29:54 -0300

    chore(db): update schema for user_stats and add user_settings table

--- internal/adapters/secondary/db/postgres/migrations/001_users.sql
+++ internal/adapters/secondary/db/postgres/migrations/001_users.sql
@@ -7,14 +7,13 @@ CREATE TABLE users (
     email VARCHAR(255) UNIQUE NOT NULL,
     -- bio VARCHAR(160), 
     -- profile_picture TEXT,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
     roles TEXT[] NOT NULL
 );
 
 CREATE TABLE user_stats (
-    id SERIAL PRIMARY KEY,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    user_id UUID PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
     
     -- Body metrics
     weight NUMERIC(5,2),
@@ -22,20 +21,40 @@ CREATE TABLE user_stats (
     body_fat_percent NUMERIC(4,1),
     
     -- Workout streaks
-    current_streak INT DEFAULT 0,
-    longest_streak INT DEFAULT 0,
+    rest_days INT NOT NULL,
+    current_streak INT NOT NULL,
+    longest_streak INT NOT NULL,
     last_workout_date DATE,
     
     -- Lifetime totals
-    total_workouts INT DEFAULT 0,
-    total_volume_lifted NUMERIC(10,2) DEFAULT 0,
-    total_time_minutes INT DEFAULT 0,
+    total_workouts INT NOT NULL,
+    total_volume_lifted NUMERIC(10,2) NOT NULL, 
+    total_time_minutes INT NOT NULL,
     
-    recorded_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 );
 
-CREATE INDEX idx_user_stats_user_id ON user_stats(user_id);
+CREATE TABLE user_settings (
+user_id UUID PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+
+    -- Display preferences 
+    preferred_weight_unit VARCHAR(2) NOT NULL,
+    preferred_height_unit VARCHAR(5) NOT NULL,
+    theme VARCHAR(10) NOT NULL,
+
+    -- Privacy 
+    profile_visibility VARCHAR(10) NOT NULL,
+
+    -- Notification preferences 
+    email_notifications BOOLEAN NOT NULL,
+    push_notifications BOOLEAN NOT NULL,
+    workout_reminders BOOLEAN NOT NUll,
+    streak_reminders BOOLEAN NOT NULL,
+
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NUll
+);
 
 -- +goose StatementBegin
 CREATE OR REPLACE FUNCTION update_timestamp() RETURNS TRIGGER AS $$
@@ -54,8 +73,12 @@ CREATE TRIGGER update_user_stats_timestamp
     BEFORE UPDATE ON user_stats
     FOR EACH ROW EXECUTE FUNCTION update_timestamp();
 
+CREATE TRIGGER update_user_settings_timestamp
+    BEFORE UPDATE ON user_settings
+    FOR EACH ROW EXECUTE FUNCTION update_timestamp();
 
 -- +goose Down
+DROP TABLE IF EXISTS user_settings;
 DROP TABLE IF EXISTS user_stats;
 DROP TABLE IF EXISTS subscriptions;
 DROP TABLE IF EXISTS users;
