6c50b302e8b6d7b9e7432ac9e17faed98403a2e5
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-06 23:29:54 -0300

    feat(user): add user stats domain model

--- internal/core/domain/user/stats.go
+++ internal/core/domain/user/stats.go
@@ -1,3 +1,28 @@
 package user
 
-type Stats struct{}
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Stats struct {
+	ID        uuid.UUID
+	Weight    *Weight
+	Height    *Height
+	BFP       *BFP
+	Streak    Streak
+	Totals    Totals
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func NewStats(id uuid.UUID, streak Streak) Stats {
+	return Stats{
+		ID:        id,
+		Totals:    NewTotals(),
+		Streak:    streak,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+}
