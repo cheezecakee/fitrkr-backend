6e8e34e830f79446767d127040ef1362022d699b
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-09 19:02:45 -0300

    feat(user): add validations to theme domain

--- internal/core/domain/user/theme.go
+++ internal/core/domain/user/theme.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"errors"
+	"strings"
 )
 
 var ErrInvalidTheme = errors.New("invalid theme")
@@ -15,6 +16,13 @@ const (
 )
 
 func NewTheme(theme string) (Theme, error) {
+	theme = strings.TrimSpace(theme)
+	theme = strings.ToLower(theme)
+
+	if theme == "" {
+		return System, nil
+	}
+
 	switch theme {
 	case "dark":
 		return Dark, nil
