ab5ba94e52ad30b818ae8f0cee382f5241c4abb6
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-11 15:53:58 -0300

    refactor(user): integrate new mutable fields and update domain logic

--- internal/core/domain/user/user.go
+++ internal/core/domain/user/user.go
@@ -8,12 +8,12 @@ import (
 )
 
 type User struct {
-	ID           uuid.UUID `json:"id"`
-	Username     Username  `json:"username"`
-	FullName     string    `json:"full_name"`
-	Email        Email     `json:"email"`
-	Password     Password  `json:"-"`
-	roles        []Role
+	ID           uuid.UUID    `json:"id"`
+	Username     Username     `json:"username"`
+	FullName     string       `json:"full_name"`
+	Email        Email        `json:"email"`
+	Password     Password     `json:"-"`
+	Roles        Roles        `json:"roles"`
 	CreatedAt    time.Time    `json:"created_at"`
 	UpdatedAt    time.Time    `json:"updated_at"`
 	Stats        Stats        `json:"stats"`
@@ -21,54 +21,18 @@ type User struct {
 	Settings     Settings     `json:"settings"`
 }
 
-func New(username Username, fullName string, email Email, roles []Role, password Password, stats Stats, subscription Subscription, settings Settings) User {
+func New(username Username, fullName string, email Email, roles Roles, password Password, stats Stats, subscription Subscription, settings Settings) User {
 	return User{
 		ID:           uuid.New(),
 		Username:     username,
 		FullName:     fullName,
 		Email:        email,
-		roles:        roles,
+		Roles:        roles,
 		Password:     password,
 		Stats:        stats,
 		Subscription: subscription,
 		Settings:     settings,
 		CreatedAt:    time.Now(),
 		UpdatedAt:    time.Now(),
-	}
-}
-
-// Roles getter
-func (u *User) Roles() Roles {
-	copyRoles := make(Roles, len(u.roles))
-	copy(copyRoles, u.roles)
-	return copyRoles
-}
-
-type UserSnapshot struct {
-	ID           uuid.UUID
-	Username     Username
-	FullName     string
-	Email        Email
-	Roles        Roles
-	Stats        Stats
-	Subscription Subscription
-	Settings     Settings
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
-}
-
-// Reconstitute rebuilds an existing user from persistence (for DB loading)
-func (s UserSnapshot) Reconstitute() *User {
-	return &User{
-		ID:           s.ID,
-		Username:     s.Username,
-		FullName:     s.FullName,
-		Email:        s.Email,
-		roles:        []Role(s.Roles),
-		Stats:        s.Stats,
-		Subscription: s.Subscription,
-		Settings:     s.Settings,
-		CreatedAt:    s.CreatedAt,
-		UpdatedAt:    s.UpdatedAt,
 	}
 }
