9153a94df1c78269a2a28240865eca837d31771c
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-11 15:53:58 -0300

    refactor(user): make totals fields mutable and clean structure

--- internal/core/domain/user/totals.go
+++ internal/core/domain/user/totals.go
@@ -1,59 +1,21 @@
 package user
 
 type Totals struct {
-	workouts int
-	volume   float64 // always stored in kg
-	time     int     // minutes
+	Workouts int
+	Lifted   float64 // always stored in kg
+	Time     int     // minutes
 }
 
 func NewTotals() Totals {
 	return Totals{
-		workouts: 0,
-		volume:   0,
-		time:     0,
+		Workouts: 0,
+		Lifted:   0,
+		Time:     0,
 	}
 }
 
-func (t Totals) AddWorkout() Totals {
-	return Totals{
-		workouts: t.workouts + 1,
-		volume:   t.volume,
-		time:     t.time,
-	}
-}
-
-func (t Totals) AddVolume(weight Weight) Totals {
-	return Totals{
-		workouts: t.workouts,
-		volume:   t.volume + weight.ToKg(),
-		time:     t.time,
-	}
-}
-
-func (t Totals) AddDuration(duration Duration) Totals {
-	return Totals{
-		workouts: t.workouts,
-		volume:   t.volume,
-		time:     t.time + duration.Minutes(),
-	}
-}
-
-func (t Totals) Workouts() int {
-	return t.workouts
-}
-
-func (t Totals) Volume() float64 {
-	return t.volume
-}
-
-func (t Totals) TimeMinutes() int {
-	return t.time
-}
-
-func (t Totals) RecordWorkout(volume Weight, duration Duration) Totals {
-	return Totals{
-		workouts: t.workouts + 1,
-		volume:   t.volume + volume.ToKg(),
-		time:     t.time + duration.Minutes(),
-	}
+func (t *Totals) RecordWorkout(lifted Weight, duration Duration) {
+	t.Workouts++
+	t.Lifted += lifted.Value
+	t.Time += duration.Minutes()
 }
