b2b8176a4b6929b50ae8dcb754eda1efde09c7f6
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-12 00:08:27 -0300

    test(users): update create_account service test for new ports and logic

--- internal/core/services/users/create_account_test.go
+++ internal/core/services/users/create_account_test.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/cheezecakee/logr"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
@@ -13,103 +12,6 @@ import (
 	"github.com/cheezecakee/fitrkr-athena/internal/core/ports"
 	"github.com/cheezecakee/fitrkr-athena/internal/core/services/users"
 )
-
-type MockUserRepo struct {
-	mock.Mock
-}
-
-func (m *MockUserRepo) Add(ctx context.Context, u user.User) error {
-	args := m.Called(ctx, u)
-	return args.Error(0)
-}
-
-func (m *MockUserRepo) GetByUsername(ctx context.Context, username string) (*ports.User, error) {
-	args := m.Called(ctx, username)
-	if args.Get(0) == nil {
-		return nil, args.Error(1)
-	}
-	return args.Get(0).(*ports.User), args.Error(1)
-}
-
-func (m *MockUserRepo) GetByEmail(ctx context.Context, email string) (*ports.User, error) {
-	args := m.Called(ctx, email)
-	if args.Get(0) == nil {
-		return nil, args.Error(1)
-	}
-	return args.Get(0).(*ports.User), args.Error(1)
-}
-
-func (m *MockUserRepo) GetByID(ctx context.Context, id string) (*ports.User, error) {
-	args := m.Called(ctx, id)
-	if args.Get(0) == nil {
-		return nil, args.Error(1)
-	}
-	return args.Get(0).(*ports.User), args.Error(1)
-}
-
-func (m *MockUserRepo) Update(ctx context.Context, u user.User) error {
-	args := m.Called(ctx, u)
-	return args.Error(0)
-}
-
-func (m *MockUserRepo) Delete(ctx context.Context, id string) error {
-	args := m.Called(ctx, id)
-	return args.Error(0)
-}
-
-func (m *MockUserRepo) AddStats(ctx context.Context, stats user.Stats, userID string) error {
-	args := m.Called(ctx, stats, userID)
-	return args.Error(0)
-}
-
-func (m *MockUserRepo) GetStatsByID(ctx context.Context, userID string) (*ports.Stats, error) {
-	args := m.Called(ctx, userID)
-	if args.Get(0) == nil {
-		return nil, args.Error(1)
-	}
-	return args.Get(0).(*ports.Stats), args.Error(1)
-}
-
-func (m *MockUserRepo) UpdateStats(ctx context.Context, stats user.Stats) error {
-	args := m.Called(ctx, stats)
-	return args.Error(0)
-}
-
-func (m *MockUserRepo) AddSubscription(ctx context.Context, sub user.Subscription, userID string) error {
-	args := m.Called(ctx, sub, userID)
-	return args.Error(0)
-}
-
-func (m *MockUserRepo) GetSubscriptionByID(ctx context.Context, userID string) (*ports.Subscription, error) {
-	args := m.Called(ctx, userID)
-	if args.Get(0) == nil {
-		return nil, args.Error(1)
-	}
-	return args.Get(0).(*ports.Subscription), args.Error(1)
-}
-
-func (m *MockUserRepo) UpdateSubscription(ctx context.Context, sub user.Subscription) error {
-	args := m.Called(ctx, sub)
-	return args.Error(0)
-}
-
-func (m *MockUserRepo) AddSettings(ctx context.Context, settings user.Settings, userID string) error {
-	args := m.Called(ctx, settings, userID)
-	return args.Error(0)
-}
-
-func (m *MockUserRepo) GetSettingsByID(ctx context.Context, userID string) (*ports.Settings, error) {
-	args := m.Called(ctx, userID)
-	if args.Get(0) == nil {
-		return nil, args.Error(1)
-	}
-	return args.Get(0).(*ports.Settings), args.Error(1)
-}
-
-func (m *MockUserRepo) UpdateSettings(ctx context.Context, settings user.Settings) error {
-	args := m.Called(ctx, settings)
-	return args.Error(0)
-}
 
 // Test helper to build valid test requests
 func validCreateAccountReq() users.CreateAccountReq {
@@ -124,7 +26,6 @@ func validCreateAccountReq() users.CreateAccountReq {
 }
 
 func TestCreateAccount(t *testing.T) {
-	logr.Init(&logr.PlainTextFormatter{}, logr.LevelInfo, nil)
 	ctx := context.Background()
 
 	tests := []struct {
