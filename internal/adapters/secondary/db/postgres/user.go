87b245ded71affbaca8064f7699191b4f0e7b8ef
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-12 12:12:39 -0300

    feat(postgres): add get methods for subscription, settings, and stats in user repository

--- internal/adapters/secondary/db/postgres/user.go
+++ internal/adapters/secondary/db/postgres/user.go
@@ -4,14 +4,12 @@ package postgres
 import (
 	"context"
 	"database/sql"
-	"time"
 
 	"github.com/cheezecakee/logr"
-	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
 
 	"github.com/cheezecakee/fitrkr-athena/internal/core/domain/user"
-	"github.com/cheezecakee/fitrkr-athena/internal/ports"
+	"github.com/cheezecakee/fitrkr-athena/internal/core/ports"
 )
 
 type UserRepo struct {
@@ -26,37 +24,11 @@ func NewUserRepo(db *sql.DB) (*UserRepo, error) {
 	}, nil
 }
 
-type UserRow struct {
-	ID        uuid.UUID
-	Username  string
-	Email     string
-	FullName  string
-	Roles     []string
-	CreatedAt time.Time
-	UpdatedAt time.Time
-}
-
-func (r UserRow) ToDomain() *user.User {
-	snapshot := user.UserSnapshot{
-		ID:           r.ID,
-		Username:     user.Username(r.Username),
-		FullName:     r.FullName,
-		Email:        user.Email(r.Email),
-		Roles:        user.StringsToRoles(r.Roles),
-		Stats:        user.Stats{},        // zero-value
-		Subscription: user.Subscription{}, // zero-value
-		Settings:     user.Settings{},     // zero-value
-		CreatedAt:    r.CreatedAt,
-		UpdatedAt:    r.UpdatedAt,
-	}
-	return snapshot.Reconstitute()
-}
-
 const CreateUser = `INSERT INTO users (id, username, full_name, email, roles, password_hash, created_at, updated_at) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)`
 
 func (ur *UserRepo) Add(ctx context.Context, u user.User) error {
 	return WithTransaction(ctx, ur.db, func(tx *sql.Tx) error {
-		_, err := tx.ExecContext(ctx, CreateUser, u.ID, u.Username, u.FullName, u.Email, u.Roles(), u.Password, u.CreatedAt, u.UpdatedAt)
+		_, err := tx.ExecContext(ctx, CreateUser, u.ID, u.Username, u.FullName, u.Email, u.Roles, u.Password, u.CreatedAt, u.UpdatedAt)
 		if err != nil {
 			return err
 		}
@@ -69,8 +41,8 @@ func (ur *UserRepo) Add(ctx context.Context, u user.User) error {
 
 const GetByUserID = `SELECT id, username, email, full_name, roles,created_at, updated_at from users WHERE id = $1`
 
-func (ur *UserRepo) GetByID(ctx context.Context, id string) (*user.User, error) {
-	var row UserRow
+func (ur *UserRepo) GetByID(ctx context.Context, id string) (*ports.User, error) {
+	var row ports.User
 
 	var rolesArray pgtype.Array[string]
 
@@ -92,13 +64,13 @@ func (ur *UserRepo) GetByID(ctx context.Context, id string) (*user.User, error) 
 
 	row.Roles = rolesArray.Elements
 
-	return row.ToDomain(), nil
+	return &row, nil
 }
 
 const GetByUsername = `SELECT id, username, email, full_name, roles,created_at, updated_at from users WHERE username = $1`
 
-func (ur *UserRepo) GetByUsername(ctx context.Context, username string) (*user.User, error) {
-	var row UserRow
+func (ur *UserRepo) GetByUsername(ctx context.Context, username string) (*ports.User, error) {
+	var row ports.User
 
 	var rolesArray pgtype.Array[string]
 
@@ -120,13 +92,13 @@ func (ur *UserRepo) GetByUsername(ctx context.Context, username string) (*user.U
 
 	row.Roles = rolesArray.Elements
 
-	return row.ToDomain(), nil
+	return &row, nil
 }
 
 const GetByUserEmail = `SELECT id, username, email, full_name, roles,created_at, updated_at from users WHERE email = $1`
 
-func (ur *UserRepo) GetByEmail(ctx context.Context, email string) (*user.User, error) {
-	var row UserRow
+func (ur *UserRepo) GetByEmail(ctx context.Context, email string) (*ports.User, error) {
+	var row ports.User
 	var rolesArray pgtype.Array[string]
 
 	err := ur.db.QueryRowContext(ctx, GetByUserEmail, email).Scan(
@@ -147,7 +119,7 @@ func (ur *UserRepo) GetByEmail(ctx context.Context, email string) (*user.User, e
 
 	row.Roles = rolesArray.Elements
 
-	return row.ToDomain(), nil
+	return &row, nil
 }
 
 // Password and roles will be updated separately at a later date
@@ -202,4 +174,146 @@ func (ur *UserRepo) Delete(ctx context.Context, id string) error {
 		logr.Get().Info("User deleted!")
 		return nil
 	})
+}
+
+const CreateUserSettings = `INSERT INTO user_settings (user_id, preferred_weight_unit, preferred_height_unit, theme, profile_visibility, email_notifications, push_notifications, workout_reminders, streak_reminders, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
+
+func (ur *UserRepo) AddSettings(ctx context.Context, us user.Settings, id string) error {
+	return WithTransaction(ctx, ur.db, func(tx *sql.Tx) error {
+		_, err := tx.ExecContext(ctx, CreateUserSettings, id, us.WeightUnit, us.HeightUnit, us.Theme, us.Visibility, us.EmailNotif, us.PushNotif, us.WorkoutReminder, us.StreakReminder, us.CreatedAt, us.UpdatedAt)
+		if err != nil {
+			return err
+		}
+
+		logr.Get().Info("New user settings created!")
+
+		return nil
+	})
+}
+
+const CreateUserStats = `INSERT INTO user_stats (user_id, rest_days, current_streak, longest_streak,  total_workouts, total_lifted, total_time_minutes, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+
+func (ur *UserRepo) AddStats(ctx context.Context, us user.Stats, id string) error {
+	return WithTransaction(ctx, ur.db, func(tx *sql.Tx) error {
+		_, err := tx.ExecContext(ctx, CreateUserStats, id, us.Streak.RestDays, us.Streak.Current, us.Streak.Longest, us.Totals.Workouts, us.Totals.Lifted, us.Totals.Time, us.CreatedAt, us.UpdatedAt)
+		if err != nil {
+			return err
+		}
+
+		logr.Get().Info("New user stats created!")
+
+		return nil
+	})
+}
+
+const CreateUserSubscription = `INSERT INTO user_subscription (user_id, plan, billing_period, started_at, auto_renew, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7)`
+
+func (ur *UserRepo) AddSubscription(ctx context.Context, us user.Subscription, id string) error {
+	return WithTransaction(ctx, ur.db, func(tx *sql.Tx) error {
+		_, err := tx.ExecContext(ctx, CreateUserSubscription, id, us.Plan, us.BillingPeriod, us.StartedAt, us.AutoRenew, us.CreatedAt, us.UpdatedAt)
+		if err != nil {
+			return err
+		}
+
+		logr.Get().Info("New user subscription created!")
+
+		return nil
+	})
+}
+
+const GetUserStats = `SELECT weight, height, body_fat_percent, rest_days, current_streak, longest_streak, last_workout_date, total_workouts, total_lifted, total_time_minutes, created_at, updated_at FROM user_stats WHERE user_id = $1`
+
+func (ur *UserRepo) GetStatsByID(ctx context.Context, userID string) (*ports.Stats, error) {
+	var row ports.Stats
+
+	err := ur.db.QueryRowContext(ctx, GetUserStats, userID).Scan(
+		&row.WeightValue,
+		&row.HeightValue,
+		&row.BFP,
+		&row.RestDays,
+		&row.Current,
+		&row.Longest,
+		&row.LastWorkout,
+		&row.TotalWorkouts,
+		&row.TotalLifted,
+		&row.TotalTime,
+		&row.CreatedAt,
+		&row.UpdatedAt,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, ports.ErrUserNotFound
+		}
+		return nil, err
+	}
+
+	return &row, nil
+}
+
+const GetUserSettings = `SELECT preferred_weight_unit, preferred_height_unit, theme, profile_visibility, email_notifications, push_notifications, workout_reminders, streak_reminders , created_at, updated_at FROM user_settings WHERE user_id = $1`
+
+func (ur *UserRepo) GetSettingsByID(ctx context.Context, userID string) (*ports.Settings, error) {
+	var row ports.Settings
+
+	err := ur.db.QueryRowContext(ctx, GetUserSettings, userID).Scan(
+		&row.WeightUnit,
+		&row.HeightUnit,
+		&row.Theme,
+		&row.Visibility,
+		&row.EmailNotifs,
+		&row.PushNotifs,
+		&row.WorkoutReminders,
+		&row.StreakReminders,
+		&row.CreatedAt,
+		&row.UpdatedAt,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, ports.ErrUserNotFound
+		}
+		return nil, err
+	}
+
+	return &row, nil
+}
+
+const GetUserSubscription = `SELECT plan, billing_period, started_at, expires_at, auto_renew, cancelled_at, last_payment_at, last_payment_amount, last_payment_currency, trial_ends_at, created_at, updated_at FROM user_subscription WHERE user_id = $1`
+
+func (ur *UserRepo) GetSubscriptionByID(ctx context.Context, userID string) (*ports.Subscription, error) {
+	var row ports.Subscription
+
+	err := ur.db.QueryRowContext(ctx, GetUserSubscription, userID).Scan(
+		&row.Plan,
+		&row.BillingPeriod,
+		&row.StartedAt,
+		&row.ExpiresAT,
+		&row.AutoRenew,
+		&row.CancelledAt,
+		&row.LastPaymentAt,
+		&row.LastPaymentAmount,
+		&row.LastPaymentCurrency,
+		&row.TrialEndsAt,
+		&row.CreatedAt,
+		&row.UpdatedAt,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, ports.ErrUserNotFound
+		}
+		return nil, err
+	}
+
+	return &row, nil
+}
+
+func (ur *UserRepo) UpdateStats(ctx context.Context, stats user.Stats) error {
+	return nil
+}
+
+func (ur *UserRepo) UpdateSubscription(ctx context.Context, sub user.Subscription) error {
+	return nil
+}
+
+func (ur *UserRepo) UpdateSettings(ctx context.Context, settings user.Settings) error {
+	return nil
 }
