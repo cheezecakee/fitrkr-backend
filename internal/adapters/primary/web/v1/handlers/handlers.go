c305e3353168d0725e1a36c6d00b5d7e4c39b959
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-15 13:22:14 -0300

    feat(web): integrate auth service into handlers

--- internal/adapters/primary/web/v1/handlers/handlers.go
+++ internal/adapters/primary/web/v1/handlers/handlers.go
@@ -1,15 +1,18 @@
 package handlers
 
 import (
+	"github.com/cheezecakee/fitrkr-athena/internal/core/services/auth"
 	"github.com/cheezecakee/fitrkr-athena/internal/core/services/users"
 )
 
 type HandlerResgistry struct {
 	UserHandler *UserHandler
+	AuthHandler *AuthHandler
 }
 
-func NewHandlerRegistry(userService users.UserService) *HandlerResgistry {
+func NewHandlerRegistry(userService users.UserService, authService auth.AuthService) *HandlerResgistry {
 	return &HandlerResgistry{
 		UserHandler: NewUserHandler(userService),
+		AuthHandler: NewAuthHandler(authService),
 	}
 }
