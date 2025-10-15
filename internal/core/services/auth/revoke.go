40e9dd896418d595d2592944fd8ca302d811fd33
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-15 13:22:14 -0300

    feat(service): add revoke service

---
+++ internal/core/services/auth/revoke.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cheezecakee/logr"
+)
+
+type RevokeTokenReq struct {
+	Token string
+}
+
+func (s *Service) Revoke(ctx context.Context, req RevokeTokenReq) error {
+	token, err := s.authRepo.GetByToken(ctx, req.Token)
+	if err != nil {
+		logr.Get().Errorf("failed to revoke token: %v", err)
+		return fmt.Errorf("failed to revoke token: %w", err)
+	}
+
+	token.Revoke()
+
+	err = s.authRepo.Update(ctx, token)
+	if err != nil {
+		logr.Get().Errorf("failed to update token: %v", err)
+		return fmt.Errorf("failed to update token: %w", err)
+	}
+
+	return nil
+}
