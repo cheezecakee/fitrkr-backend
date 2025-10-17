373a0d8abb283994911e365d6a1a8cb169f992f0
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-17 14:32:02 -0300

    fix(web): minor bug fixes in server

--- internal/adapters/primary/web/server.go
+++ internal/adapters/primary/web/server.go
@@ -7,31 +7,39 @@ import (
 
 	"github.com/go-chi/chi/v5"
 
-	v1 "github.com/cheezecakee/fitrkr-athena/internal/adapters/primary/web/v1"
+	"github.com/cheezecakee/fitrkr-athena/internal/adapters/primary/web/middleware"
+	"github.com/cheezecakee/fitrkr-athena/internal/adapters/primary/web/v1"
 	"github.com/cheezecakee/fitrkr-athena/internal/adapters/primary/web/v1/handlers"
+	"github.com/cheezecakee/fitrkr-athena/internal/adapters/secondary/external/jwt"
 	"github.com/cheezecakee/fitrkr-athena/internal/core/services/auth"
 	"github.com/cheezecakee/fitrkr-athena/internal/core/services/users"
 )
 
 type App struct {
-	chi      *chi.Mux
-	registry *handlers.HandlerResgistry
-	port     int
+	chi        *chi.Mux
+	registry   *handlers.HandlerResgistry
+	middleware *middleware.Middleware
+	jwtManager jwt.JWT
+	port       int
 }
 
-func NewApp(userService users.UserService, authService auth.AuthService, opts ...AppOption) *App {
+func NewApp(userService users.UserService, authService auth.AuthService, jwtManager jwt.JWT, opts ...AppOption) *App {
 	app := &App{
-		port: 8000,
-		chi:  chi.NewRouter(),
+		port:       8000,
+		chi:        chi.NewRouter(),
+		jwtManager: jwtManager,
 	}
 
 	for _, applyOption := range opts {
 		applyOption(app)
 	}
 
-	app.registry = handlers.NewHandlerRegistry(userService, authService)
+	app.middleware = middleware.NewMiddleware(jwtManager)
+	app.registry = handlers.NewHandlerRegistry(userService, authService, jwtManager, *app.middleware)
 
 	fs := http.FileServer(http.Dir("internal/adapters/primary/web/docs"))
+
+	app.chi.Use(app.registry.CORS)
 	app.chi.Handle("/api/v1/docs/*", http.StripPrefix("/api/v1/docs/", fs))
 
 	app.chi.Mount("/api/v1", v1.RegisterRoutes(app.registry))
