521a5b27df30403bff12fe253e16f3bf50c30da9
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-17 14:32:02 -0300

    feat(web): register auth_handler in handlers

--- internal/adapters/primary/web/v1/handlers/handlers.go
+++ internal/adapters/primary/web/v1/handlers/handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"github.com/cheezecakee/fitrkr-athena/internal/adapters/primary/web/middleware"
+	"github.com/cheezecakee/fitrkr-athena/internal/adapters/secondary/external/jwt"
 	"github.com/cheezecakee/fitrkr-athena/internal/core/services/auth"
 	"github.com/cheezecakee/fitrkr-athena/internal/core/services/users"
 )
@@ -8,11 +10,15 @@ import (
 type HandlerResgistry struct {
 	UserHandler *UserHandler
 	AuthHandler *AuthHandler
+	JwtManager  jwt.JWT
+	*middleware.Middleware
 }
 
-func NewHandlerRegistry(userService users.UserService, authService auth.AuthService) *HandlerResgistry {
+func NewHandlerRegistry(userService users.UserService, authService auth.AuthService, jwtManager jwt.JWT, middleware middleware.Middleware) *HandlerResgistry {
 	return &HandlerResgistry{
 		UserHandler: NewUserHandler(userService),
-		AuthHandler: NewAuthHandler(authService),
+		AuthHandler: NewAuthHandler(authService, jwtManager),
+		JwtManager:  jwtManager,
+		Middleware:  &middleware,
 	}
 }
