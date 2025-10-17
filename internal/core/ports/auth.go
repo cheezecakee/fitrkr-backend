2ff2137b1e3f588ac154e3869649975123dfdcbe
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-17 14:32:03 -0300

    fix(ports): resolve inconsistencies in auth ports

--- internal/core/ports/auth.go
+++ internal/core/ports/auth.go
@@ -11,8 +11,8 @@ var ErrInvalidToken = errors.New("invalid token")
 
 type AuthRepo interface {
 	Add(ctx context.Context, refreshToken auth.RefreshToken) error
-	GetByToken(ctx context.Context, token string) (auth.RefreshToken, error)
-	GetByID(ctx context.Context, userID string) ([]auth.RefreshToken, error)
+	GetByToken(ctx context.Context, token string) (*auth.RefreshToken, error)
+	GetByID(ctx context.Context, userID string) ([]*auth.RefreshToken, error)
 	Update(ctx context.Context, refreshToken auth.RefreshToken) error
 	Delete(ctx context.Context, token string) error
 }
